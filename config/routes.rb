d4b5284e61ce66feb504668e943ed7f642cdc7c0
Jessica Fuselier <jessfuse@gmaill.com>
2013-07-08 17:31:37 -0500

    added index.html and added notice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Readit::Application.routes.draw do
+  #get '/pages' => 'pages#index', as: 'pages'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +50,9 @@ Readit::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  resources :pages
+
+  root to: "pages#index"
 
   # See how all your routes lay out with "rake routes"
 
